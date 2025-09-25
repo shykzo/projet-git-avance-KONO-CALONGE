89583fdb4fcd7ec4040b5a197c885f9da5fa4b12
kvictor <victor.konomanetti@ynov.com>
2025-09-25 19:59:56 +0200

    docs(dev): Ajout de l'exemple

--- main.go
+++ main.go
@@ -1,1 +1,13 @@
-package projet_git_avance_KONO_CALONGE
+package main
+
+import "fmt"
+
+func Addition(a int, b int) int {
+	return a + b
+}
+
+func main() {
+	fmt.Println("Résultat Addition(2, 3) :", Addition(2, 3))
+}
+
+// Correction bug //
